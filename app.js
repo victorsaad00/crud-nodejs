88e419e6689c71b4e7248ed88cf91708881fcc3e
victor <victorsaad00@gmail.com>
2020-09-21 16:01:07 -0300

    Connecting DB, update Done, etc

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const app = express();
 const urlEncodeParser = bodyParser.urlencoded({extended:false});
 
 // connect to db
-const sql = mysql.createConnection({
-    host: 'localhost', 
-    user:'root',
-    password:'',
-    port:3306
+const sql = mysql.createPool({
+    user: 'b0c21b3e4b1bca',
+    pass: '87459ee4',
+    host: 'us-cdbr-east-02.cleardb.com/',
+    database: 'heroku_9b6d3ff2ea9f1ac'
 });
-
-// using db
-sql.query('use crudnodejs');
 
 app.use('/img', express.static('img'));
 app.use('/css', express.static('css'));
@@ -40,13 +37,18 @@ app.get("/", function(req, ans){
 app.get('/insert', function(req, ans){ans.render('insert');});
 
 app.get('/select:id?', function(req, ans){ 
+    
     if(!req.params.id){
-        sql.query('SELECT * FROM user ORDER BY id asc', function(err, results, fields){
-            ans.render('select', {data: results});
+        sql.getConnection(function(err, connection){
+            connection.query('SELECT * FROM user ORDER BY id asc', function(err, results, fields){
+                        ans.render('select', {data: results});
+                    });
         });
     } else {
-        sql.query('SELECT * FROM user WHERE id = ? ORDER BY id asc', [req.params.id], function(err, results, fields){
-            ans.render('select', {data: results});
+        sql.getConnection(function(err, connection){
+            connection.query('SELECT * FROM user WHERE id = ? ORDER BY id asc', [req.params.id], function(err, results, fields){
+                        ans.render('select', {data: results});
+            });
         });
     }
 });
@@ -54,7 +56,7 @@ app.get('/select:id?', function(req, ans){
 //app.get('/delete', function(req, ans){ ans.render('delete');});
 //app.get('/update', function(req, ans){ ans.render('udpate');});
 
-app.post('/controllerForm',urlEncodeParser, function(req, ans){
+app.post('/controllerForm',urlEncodeParser , function(req, ans){
     sql.query('INSERT INTO USER VALUES ( ?, ?, ?)', [req.body.id, req.body.name, req.body.age]);
     ans.render('controllerForm', {name:req.body.name});
 });
@@ -64,8 +66,19 @@ app.get('/delete/:id', function(req, res){
     res.render('delete');
 });
 
+app.get("/update/:id", function(req, ans) {
+    sql.query("SELECT * FROM user WHERE id=?", [req.params.id], function(err,results, fields){
+        ans.render('update', {id:req.params.id, name: results[0].name, age: results[0].age});
+    });
+});
+
+app.post("/controllerUpdate", urlEncodeParser, function(req, ans) {
+    sql.query("update user set name=?,age=? where id=?", [req.body.name, req.body.age, req.body.id]);
+    ans.render('controllerUpdate');
+});
 
 //start server with requiriment and answer
 app.listen(3000, function(req, ans){console.log('Server is running...');});
+
 
 
